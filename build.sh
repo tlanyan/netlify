d3ad821a291dedb62dc7773d6b40cb8e5bd9c105
tlanyan <tlanyan@hotmail.com>
2021-02-07 23:07:49 +0800

    feat: 2021.02.07

--- build.sh
+++ build.sh
@@ -5,7 +5,7 @@ sed -i'.b' 's#netlify\.app#github\.io#g' config.toml
 rm -rf *.b
 
 sed -i'.b' 's#tlanyan\.netlify\.app/images#github.com/tlanyan/netlify/raw/main/static/images#g' content/posts/*.md
-sed -i'.b' 's#tlanyan\.netlify\.app/posts#github.com/tlanyan/posts#g' content/posts/*.md
+sed -i'.b' 's#tlanyan\.netlify\.app/posts#tlanyan.github.io/posts#g' content/posts/*.md
 rm -rf content/posts/*.b
 
 hugo
